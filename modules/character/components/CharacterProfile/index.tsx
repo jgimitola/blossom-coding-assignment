97b6682b362c2e28ccb8bc7462130f5d2abb4f12
Jesus Imitola <47477274+jgimitola@users.noreply.github.com>
2024-07-07 12:23:01 -0500

    fix: add proper character profile styles

--- modules/character/components/CharacterProfile/index.tsx
+++ modules/character/components/CharacterProfile/index.tsx
@@ -3,8 +3,10 @@ import { cva } from 'class-variance-authority';
 import capitalize from 'lodash/capitalize';
 
 import type { CharacterData } from '@/character/types';
+import useCharacterStore from '@/character/zustand/characterStore';
 
 import Avatar from '@/shared/components/Avatar';
+import FilledHeartIcon from '@/shared/components/FilledHeartIcon';
 
 interface CharacterProfileProps {
   character: CharacterData;
@@ -12,30 +14,89 @@ interface CharacterProfileProps {
 
 const containerClasses = cva(['md:px-28', 'px-16', 'pt-10']);
 
+const avatarWrapperClasses = cva(['relative', 'w-fit']);
+
+const titleClasses = cva([
+  'text-2xl',
+  'font-bold',
+  'text-titles',
+  'mt-2',
+  'mb-4',
+]);
+
+const definitionClasses = cva(['border-b-[1px]', 'py-4']);
+
+const definitionLabelClasses = cva([
+  'text-base',
+  'text-titles',
+  'font-semibold',
+]);
+
+const definitionValueClasses = cva(['text-base', 'text-label', 'font-medium']);
+
+const buttonClasses = cva([
+  'font-medium',
+  'rounded-full',
+  'text-sm',
+  'p-1',
+  'inline-flex',
+  'items-center',
+  'w-8',
+  'h-8',
+  'bg-white',
+  'text-secondary.600',
+  'absolute',
+  'bottom-0',
+  '-right-3',
+]);
+
 const CharacterProfile = (props: CharacterProfileProps) => {
   const { character } = props;
+
+  // Call without use to force re-render when array values change
+  useCharacterStore((store) => store.starredCharacters);
+
+  const isCharacterStarred = useCharacterStore(
+    (store) => store.isCharacterStarred
+  );
 
   return (
     <section className={containerClasses()}>
       <div>
-        <Avatar src={character.image} className="w-[4.6875rem] h-[4.6875rem]" />
-        <h1 className="font-bold">{character.name}</h1>
+        <div className={avatarWrapperClasses()}>
+          <Avatar
+            src={character.image}
+            className="w-[4.6875rem] h-[4.6875rem]"
+          />
+          {isCharacterStarred(character.id) && (
+            <div className={buttonClasses()}>
+              <FilledHeartIcon />
+            </div>
+          )}
+        </div>
+        <h1 className={titleClasses()}>{character.name}</h1>
       </div>
 
       <dl>
-        <div>
-          <dt className="font-semibold">Specie</dt>
-          <dd>{capitalize(character.species)}</dd>
+        <div className={definitionClasses()}>
+          <dt className={definitionLabelClasses()}>Specie</dt>
+          <dd className={definitionValueClasses()}>
+            {capitalize(character.species)}
+          </dd>
         </div>
 
-        <div>
-          <dt className="font-semibold">Status</dt>
-          <dd>{capitalize(character.status)}</dd>
+        <div className={definitionClasses()}>
+          <dt className={definitionLabelClasses()}>Status</dt>
+          <dd className={definitionValueClasses()}>
+            {capitalize(character.status)}
+          </dd>
         </div>
 
-        <div>
-          <dt className="font-semibold">Gender</dt>
-          <dd>{capitalize(character.gender)}</dd>
+        <div className={definitionClasses({ className: 'border-none' })}>
+          <dt className={definitionLabelClasses()}>Gender</dt>
+          <dd className={definitionValueClasses()}>
+            {capitalize(character.gender)}
+          </dd>
         </div>
       </dl>
     </section>
